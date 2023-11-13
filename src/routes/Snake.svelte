70be117238261be79ca7b56ee70b6bfdd22ea52f
Jack <48315282+Hin97@users.noreply.github.com>
2023-11-14 00:33:33 +1100

    11/14/2023

--- src/routes/Snake.svelte
+++ src/routes/Snake.svelte
@@ -1,15 +1,39 @@
 <script lang="ts">
-	import { createApp, setAppContext, App, Graphics } from 'pixi-svelte';
+	import { createApp, setAppContext, App, Graphics, Container } from 'pixi-svelte';
+	import { onMount } from 'svelte';
+
 	const context = createApp();
 	setAppContext(context);
+
+	// Retrieving information from the Writable store
+	let sizesData: any;
+	const unsubscribe = context.canvasSizes.subscribe((value) => {
+		sizesData = value;
+	});
+
+	// Accessing specific properties (width and height)
+	const width = sizesData.width;
+	const height = sizesData.height;
 </script>
 
 <App>
-	<Graphics
-		draw={(graphics) => {
-			graphics.beginFill(0xde3249);
-			graphics.drawRect(0, 0, 150, 150);
-			graphics.endFill();
-		}}
-	/>
+	<Container>
+		<Graphics
+			draw={(graphics) => {
+				graphics.beginFill('#1099bb');
+				graphics.drawRect(0, 0, 150, 150);
+				graphics.endFill();
+			}}
+		/>
+		<Container
+			><Graphics
+				draw={(graphics) => {
+					graphics.lineStyle(2, '#000000', 1);
+					graphics.beginFill('#1099bb');
+					graphics.drawRect(1, 1, 50, 50);
+					graphics.endFill();
+				}}
+			/></Container
+		>
+	</Container>
 </App>
